7a21e17ec267921f30cba39ce753cc87087812d4
rooocket <info@rooocket>
2019-05-04 15:06:46 +0300

    Start

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo '';
+$name_bot       = 'LkFabXBot';
+$access_token   = '742869666:AAGDllupJpDtVGQ6scurdONkGhk2pJ8SIjM';
+$api            = 'https://api.telegram.org/bot' . $access_token;
+
+echo $api;
