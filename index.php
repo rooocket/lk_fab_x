84e800ba194f7e5fea033cd54b3f3f9e2403b777
rooocket <info@rooocket>
2019-05-04 18:21:24 +0300

    Start

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ $bot_token       = $_REQUEST['bot_token'];
 $bot_text        = $_REQUEST['bot_text'];
 
 $name_bot       = 'LkFabXBot';
-$site           = 'https://lkfabxbot.herokuapp.com';
-$access_token   = '742869666:AAGDllupJpDtVGQ6scurdONkGhk2pJ8SIjM';
-$api            = 'https://api.telegram.org/bot' . $access_token;
-//$api            = 'https://api.telegram.org/bot' . $bot_id . ':' . $bot_token;
+//$site           = 'https://lkfabxbot.herokuapp.com';
+//$access_token   = '742869666:AAGDllupJpDtVGQ6scurdONkGhk2pJ8SIjM';
+//$api            = 'https://api.telegram.org/bot' . $access_token;
+$api            = 'https://api.telegram.org/bot' . $bot_id . ':' . $bot_token;
 
 
 $output         = json_decode(file_get_contents($api . '/getUpdates'));
@@ -23,7 +23,7 @@ $chat_id        = $output->result[0]->message->chat->id;
 
 //Отправляем сообщение
 foreach ($chat_array as $chat) {
-    $message = empty($bot_text) ? 'Тестовое сообщение' : $bot_text;
+    $message = empty($bot_text) ? 'Новая заявка на сайте' : $bot_text;
     file_get_contents($api . '/sendMessage?chat_id=' . $chat . '&text=' . urlencode($message));
 }
 
