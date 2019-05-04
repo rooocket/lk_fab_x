c8b18537fdf8a8466bc33e0f27b9e4a7d3915106
rooocket <info@rooocket>
2019-05-04 15:28:18 +0300

    Start

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $access_token   = '742869666:AAGDllupJpDtVGQ6scurdONkGhk2pJ8SIjM';
 $api            = 'https://api.telegram.org/bot' . $access_token;
 
 echo file_get_contents($api . '/getUpdates');
-$output = json_decode(file_get_contents($api . '/getMe'));
+$output = json_decode(file_get_contents($api . '/getUpdates'));
 vd($output);
 
 
