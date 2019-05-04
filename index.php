7dbf4e27b28bc3a28ffff14abc68b989733e1046
rooocket <info@rooocket>
2019-05-04 15:16:19 +0300

    Start

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
 $name_bot       = 'LkFabXBot';
+$site           = 'https://lkfabxbot.herokuapp.com';
 $access_token   = '742869666:AAGDllupJpDtVGQ6scurdONkGhk2pJ8SIjM';
 $api            = 'https://api.telegram.org/bot' . $access_token;
 
-echo $api;
+//
+$output = json_decode(file_get_contents($GLOBALS['api'] . '/getUpdates'));
+vd($output);
+
+
+
+
+
+
+
+function vd($requerst) {
+    echo '<pre>';
+    var_dump($requerst);
+    echo '</pre>';
+}
