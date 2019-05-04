4f0dcd0a65ee589b888ca1c4af05f6fbfd7c3c0c
rooocket <info@rooocket>
2019-05-04 15:39:39 +0300

    Start

--- index.php
+++ index.php
@@ -12,7 +12,15 @@ $api            = 'https://api.telegram.org/bot' . $access_token;
 
 $output         = json_decode(file_get_contents($api . '/getUpdates'));
 $chat_id        = $output->result[0]->message->chat->id;
-echo $chat_id;
+//echo $chat_id;
+
+
+
+//Отправляем сообщение
+foreach ($chat_array as $chat) {
+    $message = 'Новая заявка на сайте';
+    file_get_contents($api . '/sendMessage?chat_id=' . $chat . '&text=' . urlencode($message));
+}
 
 
 
