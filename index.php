3204b1545984c67878f5f4c4a3c10550bbf55eab
rooocket <info@rooocket>
2019-05-04 18:16:55 +0300

    Start

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 $chat_array = array(
-    293854654
+    293854654,
+    838683004
 );
 
 $bot_id          = $_REQUEST['bot_id'];
@@ -9,10 +10,10 @@ $bot_token       = $_REQUEST['bot_token'];
 $bot_text        = $_REQUEST['bot_text'];
 
 $name_bot       = 'LkFabXBot';
-$site           = 'https://lkfabxbot.herokuapp.com';
-$access_token   = '742869666:AAGDllupJpDtVGQ6scurdONkGhk2pJ8SIjM';
-$api            = 'https://api.telegram.org/bot' . $access_token;
-//$api            = 'https://api.telegram.org/bot' . $bot_id . ':' . $bot_token;
+//$site           = 'https://lkfabxbot.herokuapp.com';
+//$access_token   = '742869666:AAGDllupJpDtVGQ6scurdONkGhk2pJ8SIjM';
+//$api            = 'https://api.telegram.org/bot' . $access_token;
+$api            = 'https://api.telegram.org/bot' . $bot_id . ':' . $bot_token;
 
 
 $output         = json_decode(file_get_contents($api . '/getUpdates'));
