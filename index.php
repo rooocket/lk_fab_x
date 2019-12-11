ec182ad7fc62467e2df244f9141de113b7e93775
rooocket <info@rooocket>
2019-12-11 10:01:46 +0300

    Compress

--- index.php
+++ index.php
@@ -10,9 +10,11 @@ $api                = 'https://api.telegram.org/bot' . $bot_id . ':' . $bot_toke
 if(!empty($getUpdates)) {
     $output          = json_decode(file_get_contents($api . '/getUpdates'));
     $chat_id         = $output->result[0]->message->chat->id;
+    echo $chat_id;
 } else {
     $message        = empty($bot_text) ? 'Новая заявка на сайте' : $bot_text;
     file_get_contents($api . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+    echo '1';
 }
 
 
