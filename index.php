3959450d35027091e168bc0f83e81cd961335664
rooocket <info@rooocket>
2019-12-21 21:06:49 +0300

    Compress

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ if(!empty($getUpdates)) {
     $chat_id         = $output->result[0]->message->chat->id;
 
     if($chat_id) {
-        echo $chat_id;
+        echo '{"response": "' . $chat_id . '"}';
     } else {
-        echo 'error_chat_id';
+        echo '{"response": "error_chat_id"}';
     }
 
 } else {
